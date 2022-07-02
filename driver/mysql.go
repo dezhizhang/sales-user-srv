52275c05d6dc3f49e84aa7ed676545d3c563dabb
dezhizhang <1541609448@qq.com>
2022-07-02 21:32:47 +0800

    feat:mysql

--- driver/mysql.go
+++ driver/mysql.go
@@ -1,1 +1,26 @@
 package driver
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"user_srv/model"
+)
+
+var DB *gorm.DB
+
+func InitDB() (err error) {
+	dsn := "root:sdf@df%%$65#fdsbXT@tcp(127.0.0.1:3306)/user_srv?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+
+		return err
+	}
+	DB = db
+	return nil
+}
+
+func init() {
+	InitDB()
+	DB.AutoMigrate(&model.User{})
+	//DB.AutoMigrate(&model.User)
+}
