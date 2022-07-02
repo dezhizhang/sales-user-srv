986c13e28c2b04f5a96e1eeb7579ae38fc2a33b7
dezhizhang <1541609448@qq.com>
2022-07-03 00:02:54 +0800

    feat:生成proto

--- model/user.go
+++ model/user.go
@@ -4,6 +4,7 @@ import "time"
 
 type User struct {
 	BaseModal
+	Name     string     `gorm:"type:varchar(100);not null" json:"name"`
 	Mobile   string     `gorm:"index:index_mobile;unique;type:varchar(11);not null" json:"mobile"`
 	Password string     `gorm:"type:varchar(100);not null" json:"password"`
 	NickName string     `gorm:"type:varchar(20)" json:"nickName"`
