b7a6dc338ccc53893febc17bca78971ef84bea4d
dezhizhang <1541609448@qq.com>
2022-07-03 09:45:39 +0800

    feat:update

--- go.mod
+++ go.mod
@@ -4,8 +4,12 @@ go 1.17
 
 require (
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	google.golang.org/grpc v1.47.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 	gorm.io/driver/mysql v1.3.4 // indirect
 	gorm.io/gorm v1.23.7 // indirect
 )
---
+++ handler/user.go
@@ -0,0 +1,67 @@
+package handler
+
+import (
+	"context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"user_srv/driver"
+	"user_srv/model"
+	"user_srv/proto"
+	"user_srv/utils"
+)
+
+type UserServer struct {
+}
+
+func ModelToResponse(user model.User) proto.UserInfoResponse {
+	userInfoResponse := proto.UserInfoResponse{
+		Id:       user.Id,
+		Mobile:   user.Mobile,
+		Password: user.Password,
+		NickName: user.NickName,
+		Name:     user.Name,
+		Gender:   user.Gender,
+		Role:     int32(user.Role),
+	}
+
+	if user.Birthday != nil {
+		userInfoResponse.Birthday = uint64(user.Birthday.Unix())
+	}
+	return userInfoResponse
+}
+
+//反回用户列表信息
+
+func (u *UserServer) GetUserList(ctx context.Context, req *proto.PageInfo) (*proto.UserListResponse, error) {
+	var users []model.User
+	result := driver.DB.Find(&users)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	rsp := &proto.UserListResponse{}
+	rsp.Total = int32(result.RowsAffected)
+	//分页
+	driver.DB.Scopes(utils.Paginate(int(req.PageIndex), int(req.PageSize))).Find(&users)
+
+	for _, user := range users {
+		userInfoResp := ModelToResponse(user)
+		rsp.Data = append(rsp.Data, &userInfoResp)
+	}
+	return rsp, nil
+
+}
+
+//通过电话查询用户信息
+
+func (u *UserServer) GetUserByMobile(ctx context.Context, req *proto.MobileRequest) (*proto.UserInfoResponse, error) {
+	var user model.User
+	result := driver.DB.Where("mobile?=", req.Mobile).Find(&user)
+	if result.RowsAffected == 0 {
+		return nil, status.Errorf(codes.NotFound, "用户不存在")
+	}
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	userInfoRsp := ModelToResponse(user)
+	return &userInfoRsp, nil
+}
